1d37943112c4a0b2ce39b507bcb9b61e43aba636
engineerjoe440 <engineerjoe440@yahoo.com>
2021-08-07 09:49:46 -0700

    based backend

---
+++ backend/main.py
@@ -0,0 +1,31 @@
+################################################################################
+"""
+DJ JOE Website Availability Calendar
+------------------------------------
+
+(c) 2021 - Stanley Solutions - Joe Stanley
+
+This application serves the React frontend required to demonstrate the available
+dates for DJ Joe Services.
+"""
+################################################################################
+
+# Requirements
+from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+
+# Application Base
+app = FastAPI()
+
+# Mount the Static File Path
+app.mount("/static", StaticFiles(directory="static"), name="static")
+templates = Jinja2Templates(directory="templates")
+
+# Main Application Response
+@app.get("/")
+async def root():
+    return templates.TemplateResponse(
+        "item.html",
+        {},
+    )
