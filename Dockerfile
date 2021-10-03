9ad18d9e1ea3968f1ef25efb5d8a519f09cd9acf
engineerjoe440 <engineerjoe440@yahoo.com>
2021-10-03 11:35:15 -0700

    updated google calendar handling, added logging opts

--- Dockerfile
+++ Dockerfile
@@ -10,4 +10,4 @@ COPY ../secrets /server/app/secrets
 RUN pip install --no-cache-dir --upgrade -r /server/app/requirements.txt
 
 # Run Server on localhost:8383 so Nginx can Hit it without Direct Extern. Access
-CMD ["uvicorn", "app.main:app", "--host", "127.0.0.1", "--port", "8383"]
+CMD ["uvicorn", "app.main:app", "--host", "127.0.0.1", "--port", "8383", "--log-config", "log_conf.yml"]
--- backend/google_calendar.py
+++ backend/google_calendar.py
@@ -10,70 +10,55 @@ dates for DJ Joe Services.
 """
 ################################################################################
 
+# Import Requisites
+import os
 import datetime
-import os.path
+import requests
 from date_support import daterange, _clean_dates, _restore_datetimes
-from googleapiclient.discovery import build
-from google_auth_oauthlib.flow import InstalledAppFlow
-from google.auth.transport.requests import Request
-from google.oauth2.credentials import Credentials
 
-# If modifying these scopes, delete the file token.json.
-SCOPES = ['https://www.googleapis.com/auth/calendar.readonly']
+ENV_API_KEY = "GOOGLE_API_KEY"
 
-def google_calendar_service_available():
-    """Quickly Determines if Token is Available for Google Calendar System."""
-    return os.path.exists('token.json')
+BASE_URL = (
+    "https://clients6.google.com/calendar/v3/calendars/engineerjoe440@gmail.com"
+    "/events?calendarId=engineerjoe440%40gmail.com&singleEvents=true&timeZone="
+    "America%2FLos_Angeles&maxAttendees=1&maxResults=250&sanitizeHtml=true&"
+    "timeMin={TIME_MIN}&timeMax={TIME_MAX}&key={API_KEY}"
+)
+
+################################################################################
+# Supporting Functions
+
+def googlify_datetimes(dts):
+    dts = _restore_datetimes(_clean_dates(dts))
+    return [dt.isoformat()+"Z" for dt in dts]
 
 def get_google_datetime(google_dt_dict):
     """Performs dictionary-specific handling to attempt extraction of dt."""
     google_dt = google_dt_dict.get('dateTime', google_dt_dict.get('date'))
     return google_dt.split("T")[0]
 
-def get_service():
-    """Gets a handle to the Google Calendar Service."""
-    creds = None
-    # The file token.json stores the user's access and refresh tokens, and is
-    # created automatically when the authorization flow completes for the first
-    # time.
-    if google_calendar_service_available():
-        creds = Credentials.from_authorized_user_file('token.json', SCOPES)
-    # If there are no (valid) credentials available, let the user log in.
-    if not creds or not creds.valid:
-        if creds and creds.expired and creds.refresh_token:
-            creds.refresh(Request())
-        else:
-            flow = InstalledAppFlow.from_client_secrets_file(
-                'credentials.json', SCOPES)
-            creds = flow.run_local_server(port=0)
-        # Save the credentials for the next run
-        with open('token.json', 'w') as token:
-            token.write(creds.to_json())
-
-    service = build('calendar', 'v3', credentials=creds)
-
-    return service
-
-def googlify_datetimes(dts):
-    dts = _restore_datetimes(_clean_dates(dts))
-    return [dt.isoformat()+"Z" for dt in dts]
+################################################################################
+# Event Listing Functions
 
 def get_event_list(start: datetime.datetime, end: datetime.datetime):
     """Identifies a list of all events in the specified date range."""
-    service = get_service()
-
     start, end = googlify_datetimes([start, end])
 
     # Call the Calendar API
-    events_result = service.events().list(calendarId='primary', timeMin=start,
-                                          timeMax=end, singleEvents=True,
-                                          orderBy='startTime').execute()
-    return events_result.get('items', [])
+    REQ_URL = BASE_URL.format(
+        TIME_MIN = start,
+        TIME_MAX = end,
+        API_KEY = os.getenv(ENV_API_KEY),
+    )
+    print(REQ_URL)
+    resp = requests.get(REQ_URL)
+    if resp.status_code == 200:
+        return resp.json().get('items', [])
+    else:
+        return []
 
 def get_occupied_dates(start: datetime.datetime, end: datetime.datetime):
     """Generates a list of single dt objects representing occupied dates."""
-    if not google_calendar_service_available():
-        return [] # Bail Out without Attempting Connection
     events = get_event_list(start=start, end=end)
     occupied_dates = []
 
@@ -107,7 +92,6 @@ if __name__ == '__main__':
     if not events:
         print('No upcoming events found.')
     for event in events:
-        start = event['start'].get('dateTime', event['start'].get('date'))
-        print(start, event['summary'])
+        print(event['start'].get('dateTime', event['start'].get('date')))
     
     print(get_occupied_dates(now, now + datetime.timedelta(days=30)))
